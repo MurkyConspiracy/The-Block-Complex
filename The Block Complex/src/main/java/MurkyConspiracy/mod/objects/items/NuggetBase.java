3a8d452b946ae35cd2d7162d73ec1224b8abc756
Christopher Magee <frostydknight@gmail.com>
2019-01-23 20:47:44 -0800

    push maybe

--- The Block Complex/src/main/java/MurkyConspiracy/mod/objects/items/NuggetBase.java
+++ The Block Complex/src/main/java/MurkyConspiracy/mod/objects/items/NuggetBase.java
@@ -1,44 +1,68 @@
 package MurkyConspiracy.mod.objects.items;
 
 import MurkyConspiracy.mod.Main;
+import MurkyConspiracy.mod.init.BlockInit;
 import MurkyConspiracy.mod.init.ItemInit;
 import MurkyConspiracy.mod.util.interfaces.IHasModel;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 
 public class NuggetBase extends Item implements IHasModel
 {
 	
 	public NuggetBase(String name, Boolean hasIngot)
 	{
+		String nuggetName = name + "_nugget";
 		
-		setUnlocalizedName(name);
-		setRegistryName(name);
+		setUnlocalizedName(nuggetName);
+		setRegistryName(nuggetName);
 		setCreativeTab(Main.theblockcomplextab);
 		
 		ItemInit.ITEMS.add(this);
 		
 		if(hasIngot)
 		{
-			/*add ingot implementations*/
+			String ingotName = "Ingot_" + name;
+			
+			setUnlocalizedName(ingotName);
+			setRegistryName(ingotName);
+			setCreativeTab(Main.theblockcomplextab);
+			
+			ItemInit.ITEMS.add(this);
 		}
 		
 	}
 	
 	public NuggetBase(String name, Boolean hasIngot, Material material)
 	{
+		String nuggetName = name = "_nugget";
+		String oreName = name = "_ore";
 		
-		setUnlocalizedName(name);
-		setRegistryName(name);
+		setUnlocalizedName(nuggetName);
+		setRegistryName(nuggetName);
 		setCreativeTab(Main.theblockcomplextab);
 		
 		ItemInit.ITEMS.add(this);
 		
 		if(hasIngot)
 		{
-			/*add ingot implementations*/
+			String ingotName = "Ingot_" + name;
+			
+			setUnlocalizedName(ingotName);
+			setRegistryName(ingotName);
+			setCreativeTab(Main.theblockcomplextab);
+			
+			ItemInit.ITEMS.add(this);
 		}
-		/* add block implementations*/
+		
+		super(material);
+		setUnlocalizedName(oreName);
+		setRegistryName(oreName);
+		setCreativeTab(Main.theblockcomplextab);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 	}
 
 	@Override
