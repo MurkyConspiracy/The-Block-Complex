89be69d5ec55d7032de942199eb776eee2b1a000
MurkyConspiracy <nickreeder43@gmail.com>
2019-01-24 18:22:17 -0600

    Added NuggetIngot combo, working on block

--- The Block Complex/src/main/java/MurkyConspiracy/mod/init/ItemInit.java
+++ The Block Complex/src/main/java/MurkyConspiracy/mod/init/ItemInit.java
@@ -3,9 +3,8 @@ package MurkyConspiracy.mod.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.material.Material;
 import MurkyConspiracy.mod.objects.items.ItemBase;
-import MurkyConspiracy.mod.objects.items.NuggetBase; 
+import MurkyConspiracy.mod.objects.items.complex.NuggetIngotBlock;
 import net.minecraft.item.Item;
 
 public class ItemInit 
@@ -13,8 +12,9 @@ public class ItemInit
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item decay_food = new ItemBase("decay_food");
+	public static final Item copper = new NuggetIngotBlock("copper");
 	
-	public static final Item copper = new NuggetBase("copper", false);
+	//public static final Item copper = new NuggetBase("copper", false);
 /*	public static final Item ingot_tin = new ItemBase("ingot_tin");
 	public static final Item ingot_lead = new ItemBase("ingot_lead");
 	public static final Item ingot_zinc = new ItemBase("ingot_zinc");
---
+++ The Block Complex/src/main/java/MurkyConspiracy/mod/objects/items/complex/NuggetIngotBlock.java
@@ -0,0 +1,112 @@
+package MurkyConspiracy.mod.objects.items.complex;
+
+import MurkyConspiracy.mod.Main;
+import MurkyConspiracy.mod.init.ItemInit;
+import MurkyConspiracy.mod.util.interfaces.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+public class NuggetIngotBlock extends Item implements IHasModel
+{
+
+	private boolean iHasNugget, iHasIngot, iHasBlock;
+	public NuggetIngotBlock(String name)
+	{
+
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setHasSubtypes(true);
+		setMaxDamage(0);
+		setCreativeTab(Main.theblockcomplextab);
+		
+		ItemInit.ITEMS.add(this);
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+	{
+		if(this.isInCreativeTab(tab))
+		{
+			for(int i = 0; i < 2; ++i)
+			{
+				items.add(new ItemStack(this, 1, i));
+			}
+		}
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack)
+	{
+		int i = stack.getMetadata();
+		return super.getUnlocalizedName() + "_" + NIB.byNIBMeta(i).getUnlocalizedName();
+	}
+
+	@Override
+	public void registerModels()
+	{
+		for(int i = 0; i < 2; ++i)
+		{
+			Main.proxy.registerItemRenderer(this, i, "inventory");
+		}
+	}
+	
+}
+
+enum NIB
+{
+	NUGGET(0, "nugget"),
+	INGOT(1, "ingot");
+	
+	private final int meta;
+
+	private final String name, unlocalizedName;
+
+    private static final NIB[] META_LOOKUP = new NIB[values().length];
+	
+	private NIB(int meta, String name)
+	{
+		this(meta, name, name);
+	}
+	
+	private NIB(int meta, String name, String unlocalizedName)
+	{
+		this.meta = meta;
+		this.name = name;
+		this.unlocalizedName = unlocalizedName;
+	}
+	
+	public String getName()
+	{
+		return name;		
+	}
+	
+	static
+    {
+        for (NIB NugIngBloc : values())
+        {
+            META_LOOKUP[NugIngBloc.getMetadata()] = NugIngBloc;
+        }
+    }
+
+	private int getMetadata()
+	{
+		return this.meta;
+	}
+	
+	public static NIB byNIBMeta(int damage)
+    {
+        if (damage < 0 || damage >= META_LOOKUP.length)
+        {
+            damage = 0;
+        }
+
+        return META_LOOKUP[damage];
+    }
+	public String getUnlocalizedName()
+    {
+        return this.unlocalizedName;
+    }
+}
