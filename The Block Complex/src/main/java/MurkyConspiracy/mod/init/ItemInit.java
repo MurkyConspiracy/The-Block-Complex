7d5d00eb866e10a33ae3feb127bf87deb7bd2250
Nicholas Reeder <castofoblivion@live.com>
2019-01-27 20:35:00 -0600

    Remembered I was avoiding common gems...

--- The Block Complex/src/main/java/MurkyConspiracy/mod/init/ItemInit.java
+++ The Block Complex/src/main/java/MurkyConspiracy/mod/init/ItemInit.java
@@ -13,17 +13,6 @@ public class ItemInit
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item decay_food = new ItemBase("decay_food");
-	public static final Item copper = new NuggetIngotBlock("copper");
-	
-	public static final Item ruby = new GemCuts("ruby");
-	//public static final Item copper = new NuggetBase("copper", false);
-/*	public static final Item ingot_tin = new ItemBase("ingot_tin");
-	public static final Item ingot_lead = new ItemBase("ingot_lead");
-	public static final Item ingot_zinc = new ItemBase("ingot_zinc");
-	public static final Item ingot_nickel = new ItemBase("ingot_nickel");
-	public static final Item ingot_platinum = new ItemBase("ingot_platinum");
-	public static final Item ingot_tungsten = new ItemBase("ingot_tungsten");*/
-	
-	
+	public static final Item copper = new NuggetIngotBlock("copper");	
 	
 }
