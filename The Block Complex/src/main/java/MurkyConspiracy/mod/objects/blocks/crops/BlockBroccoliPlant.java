0f5f8e54fcb90f0f69bc4e091ea552d5677c4ae6
MurkyConspiracy <nickreeder43@gmail.com>
2019-01-29 20:47:47 -0600

    Fixed texturing issue with multistate blocks

--- The Block Complex/src/main/java/MurkyConspiracy/mod/objects/blocks/crops/BlockBroccoliPlant.java
+++ The Block Complex/src/main/java/MurkyConspiracy/mod/objects/blocks/crops/BlockBroccoliPlant.java
@@ -20,7 +20,7 @@ import net.minecraft.world.World;
 
 public class BlockBroccoliPlant extends BlockCrops
 {
-	private static final AxisAlignedBB[] BROCCOLI_AABB = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.1875D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.3125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.4375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5625D, 1.0D)};
+	private static final AxisAlignedBB[] BROCCOLI_AABB = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.1875D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.3125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D)};
 
 	private Random rand = new Random();
 	
